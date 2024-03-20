884d79bdced2d462a59f4135ecdb72618e180f97
arvindinfo5 <54007010+arvindinfo5@users.noreply.github.com>
2024-03-20 19:12:14 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,77 @@
+"use strict"; // teat all js code as new version
+
+// alert(3 + 3)  // we are using nodejs, not brower
+
+console.log(4 + 4)
+
+console.log("Arvind")
+
+
+let name = "arvind"
+let age = 18
+let isLoggedIn = false
+
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined
+// symbol => unique
+
+
+// object
+
+console.log(typeof "");
+console.log(typeof null); // undefined
+console.log(typeof undefined); // object
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
